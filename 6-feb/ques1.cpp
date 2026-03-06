b95bce6680ae65b238d2a4342715e905a820fea6
Anjali <anjali1442005@gmail.com>
2026-03-06 10:32:22 +0530

    6_feb

---
+++ 6-feb/ques1.cpp
@@ -0,0 +1,24 @@
+/*calculate the max sum of k size subarray */
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int n,k;
+    cin>>n>>k;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+
+    }
+    int max_sum=a[0];
+    for(int i=0;i<k;i++){
+        int sum=0;
+        for(int j=i;j<i+k;j++){
+            sum+=a[j];
+        }
+        max_sum=max(max_sum,sum);
+
+    }
+    return max_sum;
+    
+}
